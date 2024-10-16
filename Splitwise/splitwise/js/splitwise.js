0d69b9c2b64ece537a464b8234cc206222aea80f
felix18485 <felix18485@gmail.com>
2024-10-16 10:50:08 +0200

    Creacion de formulario

--- Splitwise/splitwise/js/splitwise.js
+++ Splitwise/splitwise/js/splitwise.js
@@ -17,3 +17,43 @@ class Gasto {
     }
     // Completar con los métodos necesarios
 }
+
+//Accedemos al contenedor que contendra el formulario por su id
+let contenedor = document.getElementById("accordion-body");
+//Creamos el formulario
+let labelUsuario = document.createElement("label");
+labelUsuario.textContent = "Usuarios";
+let formulario = document.createElement("form");
+//Creamos el menu desplegable con las opciones
+let usuarios = document.createElement("select");
+let opcionVacia = document.createElement("option");
+opcionVacia.setAttribute("value", "---");
+opcionVacia.text = "---";
+let opcionJuan = document.createElement("option");
+opcionJuan.setAttribute("value", "Juan");
+opcionJuan.textContent = "Juan";
+let opcionMaria = document.createElement("option");
+opcionMaria.setAttribute("value", "Maria");
+opcionMaria.textContent = "Maria";
+let opcionAna = document.createElement("option");
+opcionAna.setAttribute("value", "Ana");
+opcionAna.textContent = "Ana";
+//Creamos el input para el  titulo
+let labelTitulo = document.createElement("label");
+labelTitulo.textContent = "Titulo";
+let inputTitulo = document.createElement("input");
+inputTitulo.setAttribute("type", "text");
+//Creamos el input para el importe
+let labelImporte = document.createElement("label");
+labelImporte.textContent = "Importe";
+let inputImporte = document.createElement("input");
+inputImporte.setAttribute("type", "text");
+//Creamos el input para la fecha
+let labelFecha = document.createElement("label");
+labelFecha.textContent = "Fecha";
+let inputFecha = document.createElement("input");
+inputFecha.setAttribute("type", "text");
+//Añadimos todos los campos al formulario
+contenedor.append(formulario);
+usuarios.append(opcionVacia,opcionJuan,opcionMaria,opcionAna);
+formulario.append(labelUsuario,usuarios,labelTitulo,inputTitulo,labelImporte,inputImporte,labelFecha,inputFecha);
